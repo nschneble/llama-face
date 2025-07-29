7c6799cae0f26905792eb20d87620974787c90f4
Nick Schneble <nschneble@users.noreply.github.com>
2025-07-29 15:02:08 -0400

    add basic server code

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('Server is running!');
+});
+
+app.listen(port, () => {
+	console.log(`Server listening at http://localhost:${port}`);
+});
